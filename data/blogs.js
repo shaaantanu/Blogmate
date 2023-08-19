1d6fb6475fef41833ed8ea991fb4ffb215fb097e
vinaypoddar2000 <73072291+vinaypoddar402@users.noreply.github.com>
2023-08-19 18:23:40 +0530

    Second_Commit

--- data/blogs.js
+++ data/blogs.js
@@ -1,19 +1,19 @@
 blogs = [
     {
         title: "American Education System",
-        content: "There is a lot more that should be done to improve...",
+        content: "https://en.wikipedia.org/wiki/Education_in_the_United_States",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque deserunt ipsa modi explicabo provident quia ex incidunt blanditiis doloremque dolor ullam, dolore consequuntur in aliquam numquam nisi, error inventore aspernatur, corrupti magni aut laboriosam? Cumque, enim rerum impedit ad tenetur cum assumenda ipsa perferendis mollitia modi? Omnis, vel voluptatibus.",
         slug: "america-education"
     },
     {
         title: "Finland Education System",
-        content: "There is a reason why this is considered to be the best in the world...",
+        content: "https://en.wikipedia.org/wiki/Education_in_Finland",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque deserunt ipsa modi explicabo provident quia ex incidunt blanditiis doloremque dolor ullam, dolore consequuntur in aliquam numquam nisi, error inventore aspernatur, corrupti magni aut laboriosam? Cumque, enim rerum impedit ad tenetur cum assumenda ipsa perferendis mollitia modi? Omnis, vel voluptatibus.",
         slug: "finland-education"
     },
     {
         title: "Indian Education System",
-        content: "Adopted for a long time but, definitely needs improvement...",
+        content: "https://en.wikipedia.org/wiki/Education_in_India",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque deserunt ipsa modi explicabo provident quia ex incidunt blanditiis doloremque dolor ullam, dolore consequuntur in aliquam numquam nisi, error inventore aspernatur, corrupti magni aut laboriosam? Cumque, enim rerum impedit ad tenetur cum assumenda ipsa perferendis mollitia modi? Omnis, vel voluptatibus.",
         slug: "india-education"
     }
